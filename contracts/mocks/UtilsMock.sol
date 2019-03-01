6b5af5ffac17e5f9a13b2d07462e965e831d8bec
RookieLinLucy <44455149+RookieLinLucy@users.noreply.github.com>
2019-03-01 09:11:14 +0800

    unit_test (#10)
    * disable whitelist
    
    Signed-off-by: ericnien <nien0223@gmail.com>
    
    * [Add version field to distinguish same requestId]
    
    * test lib
    
    * update bn256_test.js&utils_test.js
    
    * solve warning(BN256Mock)&try testing DOSOnChainSDK.sol
    
    * update proxy_test(unfinished)
    
    * tinker
    
    * update dosproxy_test (unfinished)
    
    * update dosproxy_test(unfinished:events watch)
    
    * complete dosproxy_test
    
    * clean proxy
    
    * remove useless change

---
+++ contracts/mocks/UtilsMock.sol
@@ -0,0 +1,73 @@
+pragma solidity >= 0.4.24;
+import "../lib/utils.sol";
+
+contract UtilsMock {
+    function returnUINT256MAX() public pure returns(uint) {
+        return ~uint(0);
+    }
+
+    function createByte() public pure returns(byte) {
+        return '6';
+    }
+
+    function byte2Uint(byte b) public pure returns(uint8) {
+        return utils.byte2Uint(b);
+    }
+
+    function hexByte2Uint(byte b) public pure returns(uint8) {
+        return utils.hexByte2Uint(b);
+    }
+
+    function str2Uint(string memory a) public pure returns(uint) {
+        return utils.str2Uint(a);
+    }
+
+    function hexStr2Uint(string memory a) public pure returns(uint) {
+        return utils.hexStr2Uint(a);
+    }
+
+    function str2Addr(string memory a) public pure returns(address) {
+        return utils.str2Addr(a);
+    }
+
+    function uint2HexStr(uint x) public pure returns(string memory) {
+        return utils.uint2HexStr(x);
+    }
+
+    function uint2Str(uint x) public pure returns(string memory) {
+        return utils.uint2Str(x);
+    }
+
+    function addr2Str(string memory a) public pure returns(string memory) {
+        address x = utils.str2Addr(a);
+        return utils.addr2Str(x);
+    }
+
+    function bytesConcat(bytes memory a, bytes memory b) public pure returns(bytes memory) {
+        return utils.bytesConcat(a,b);
+    }   
+
+    function strConcat(string memory a, string memory b) public pure returns(string memory) {
+        return utils.strConcat(a,b);
+    }
+
+    function strCompare(string memory a, string memory b) public pure returns(int) {
+        return utils.strCompare(a, b);
+    }
+
+    function strEqual(string memory a, string memory b) public pure returns(bool) {
+        return utils.strEqual(a, b);
+    }
+
+    function indexOf(string memory haystack, string memory needle) public pure returns(int) {
+        return utils.indexOf(haystack, needle);
+    }
+
+    function subStr(string memory a, uint start, uint len) public pure returns(string memory) {
+        return utils.subStr(a, start, len);
+    }
+
+    function subStr1(string memory a, uint start) public pure returns(string memory) {
+        return utils.subStr(a, start);
+    }
+}
