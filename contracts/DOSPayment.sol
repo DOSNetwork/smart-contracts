671002e304f4f7a150b24614ae7b586968dfca6f
jonnyhsy <hsyjonny@gmail.com>
2019-05-07 15:25:22 -0700

    update payment conttract staking part.

--- contracts/DOSPayment.sol
+++ contracts/DOSPayment.sol
@@ -14,45 +14,46 @@ contract ERC20I {
 contract DOSPayment is Ownable {
     // DOS Token on rinkeby testnet
     address public networkToken = 0x214e79c85744CD2eBBc64dDc0047131496871bEe;
-    address public droplockToken;
+    address public dropburnToken;
     uint public minStake = 50000;  // Minimum number of tokens required to be eligible into the protocol network.
-    uint public droplockMaxQuota = 3;  // Each droplock quota reduces 10% of minStake requirement to participateb into protocol.
+    uint public dropburnMaxQuota = 3;  // Each DropBurn quota reduces 10% of minStake requirement to participate into protocol.
     
     event UpdateNetworkTokenAddress(address oldAddress, address newAddress);
-    event UpdateDroplockTokenAddress(address oldAddress, address newAddress);
-    event UpdateDroplockMaxQuota(uint oldQuota, uint newQuota);
+    event UpdateDropBurnTokenAddress(address oldAddress, address newAddress);
+    event UpdateDropBurnMaxQuota(uint oldQuota, uint newQuota);
     
     function setNetworkToken(address addr) public onlyOwner {
         emit UpdateNetworkTokenAddress(networkToken, addr);
         networkToken = addr;
     }
     
-    function setDroplockToken(address addr) public onlyOwner {
-        emit UpdateDroplockTokenAddress(droplockToken, addr);
-        droplockToken = addr;
+    function setDropBurnToken(address addr) public onlyOwner {
+        emit UpdateDropBurnTokenAddress(dropburnToken, addr);
+        dropburnToken = addr;
     }
 
-    function setDroplockMaxQuota(uint quo) public onlyOwner {
-        require(quo != droplockMaxQuota && quo < 10, "Valid droplockMaxQuota within 0 to 9");
+    function setDropBurnMaxQuota(uint quo) public onlyOwner {
+        require(quo != dropburnMaxQuota && quo < 10, "Valid dropburnMaxQuota within 0 to 9");
         
-        emit UpdateDroplockMaxQuota(droplockMaxQuota, quo);
-        droplockMaxQuota = quo;
+        emit UpdateDropBurnMaxQuota(dropburnMaxQuota, quo);
+        dropburnMaxQuota = quo;
     }
 
     // TODO: Rewrite eligibility and staking algorithm.
     function fromValidStakingNode(address node) public view returns(bool) {
         uint networkTokenBalance = ERC20I(networkToken).balanceOf(node);
         uint networkTokenDecimals = ERC20I(networkToken).decimals();
-        if (networkTokenBalance >= minStake * networkTokenDecimals) {
+        uint minNetworkStakingBalance = minStake * (10 ** networkTokenDecimals);
+        if (networkTokenBalance >= minNetworkStakingBalance) {
             return true;
-        } else if (droplockToken == address(0x0)) {
+        } else if (dropburnToken == address(0x0)) {
             return false;
         } else {
-            uint droplockTokenNum = ERC20I(droplockToken).balanceOf(node) / ERC20I(droplockToken).decimals();
-            if (droplockTokenNum > droplockMaxQuota) {
-                droplockTokenNum = droplockMaxQuota;
+            uint dropburnTokenNum = ERC20I(dropburnToken).balanceOf(node) / (10 ** ERC20I(dropburnToken).decimals());
+            if (dropburnTokenNum > dropburnMaxQuota) {
+                dropburnTokenNum = dropburnMaxQuota;
             }
-            return networkTokenBalance >= minStake * networkTokenDecimals * (10 - droplockTokenNum) / 10;
+            return networkTokenBalance >= minNetworkStakingBalance * (10 - dropburnTokenNum) / 10;
         }
     }
     
